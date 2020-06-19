2ce5b7501bcb0740b3bb7d842b1520e5b89e5815
sridhar02 <kattasridhar02@gmail.com>
2020-06-19 08:20:26 +0530

    main App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Pokemons from "./components/pokemons";
+import { makeStyles } from "@material-ui/core";
+
+const useAppStyles = makeStyles((theme) => ({
+  App: {
+  },
+}));
 
 function App() {
+  const classes = useAppStyles();
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className={classes.App}>
+      <Pokemons />
     </div>
   );
 }
