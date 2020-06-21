5c2f617fdfcba3403f187c8fde347b28d9e5c1aa
sridhar02 <kattasridhar02@gmail.com>
2020-06-21 11:47:32 +0530

    delete pokemon tests update

--- cypress/integration/deletePokemon.spec.js
+++ cypress/integration/deletePokemon.spec.js
@@ -1,18 +1,25 @@
-describe("Add pokemon tests", () => {
+describe("delete pokemon tests", () => {
   before(() => {
     cy.visit("/");
   });
+
   const apiUrl = Cypress.env("apiUrl");
-  it("select a pokemon from the list and update the details", () => {
+
+  it("select a pokemon from the list and delete the pokemon", () => {
     cy.server();
+
     cy.route("DELETE", `${apiUrl}/pokemons/*`).as("delete-pokemon");
+
     cy.route("GET", `${apiUrl}/pokemons`).as("getPokemons");
+
     cy.contains("Meltan").click();
+
     cy.get('button[id="confimation"]').click();
 
     cy.get('button[id="delete"]').click();
 
     cy.wait("@delete-pokemon").should("have.property", "status", 200);
+
     cy.wait("@getPokemons").should("have.property", "status", 200);
   });
 });
