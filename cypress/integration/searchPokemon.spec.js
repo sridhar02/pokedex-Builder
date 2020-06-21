f0f35f313a190d500cb75a2eb4c086cc7925b132
sridhar02 <kattasridhar02@gmail.com>
2020-06-21 12:40:55 +0530

    cypress test results and tests

--- cypress/integration/searchPokemon.spec.js
+++ cypress/integration/searchPokemon.spec.js
@@ -1,7 +1,13 @@
-it("select a pokemon from the list and update the details", () => {
-  cy.visit("/");
+describe("testing select a pokemon", () => {
+  before(() => {
+    cy.visit("/");
+  });
 
-  cy.get('input[id="search"]').type("Charizard");
+  it("select a pokemon from the list and update the details", () => {
+    cy.get('input[id="search"]').type("Charizard");
+    // cy.get('select').select('Charizard') // Select the 'user-1' option
+    // cy.contains("Charizard").click();
 
-  cy.get('input[variant="outlined"]').click()
+  });
+
 });
--- cypress/integration/selectPokemon.spec.js
+++ cypress/integration/selectPokemon.spec.js
@@ -1,14 +1,13 @@
-describe("Testing addition of a pokemon", () => {
+describe("testing select a pokemon", () => {
   before(() => {
     cy.visit("/");
   });
 
   it("select a pokemon from the list and update the details", () => {
     cy.contains("Charizard").click();
-    cy.get('#name').should('be.visible')
-    cy.contains('Charizard').should('be.visible')
-    cy.contains('Fire').should('be.visible')
-    cy.contains('Flying').should('be.visible')
-
+    cy.get("#name").should("be.visible");
+    cy.contains("Charizard").should("be.visible");
+    cy.contains("Fire").should("be.visible");
+    cy.contains("Flying").should("be.visible");
   });
 });
